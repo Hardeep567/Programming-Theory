b2b9d0519bb08473c22270630030f05452baa671
Hardeep567 <hardeepnaresh8@gmail.com>
2024-11-15 23:56:09 -0800

    Create code.c

---
+++ Assingnment 2/Problem 10/code.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int main() {
+    int num, i, j;
+
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    for (i = 1; i <= num-1; i++) {
+        for (j= 1;  j<= (num-i) * 2; j++) {
+            printf(" ");
+        }
+        printf("*");
+        if (i> 1) {
+            for (j= 1; j<= (i-1) * 2 - 1; j++) {
+                printf(" ");
+            }
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (j= 1; j<= num; j++){
+        printf("*");
+    }
+    for (j= 1; j<= (num-1) * 2 - 1; j++){
+        printf(" ");
+    }
+    for (j= 1; j<= num; j++){
+        printf("*");
+    }
+    printf("\n");
+    for (i= num - 1; i>= 1; i--) {
+        for (j= 1; j<= (num-i); j++) {
+            printf("  ");
+        }
+        printf("*");
+        for (j= 1; j<= (i-1) * 2 - 1; j++) {
+            printf(" ");
+        }
+        if (i> 1) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
